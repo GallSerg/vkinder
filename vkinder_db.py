7792d25dcb4da847c8da60756fec756253a0e14a
e12io898 <e12io898@ya.ru>
2023-07-01 18:31:41 +0300

    create model database 3 red

--- vkinder_db.py
+++ vkinder_db.py
@@ -17,30 +17,39 @@ def connect_db():
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    user_id = sq.Column(sq.Integer, primary_key=True)
+    firs_name = sq.Column(sq.Text, nullable=False)
+    last_name = sq.Column(sq.Text, nullable=False)
+    city = sq.Column(sq.Text, nullable=False)
+    gender = sq.Column(sq.Text, nullable=False)
+    age = sq.Column(sq.Integer, nullable=False)
+
 class History(Base):
     __tablename__ = 'history'
 
-    id = sq.Column(sq.Integer, primary_key=True)
-    link = sq.Column(sq.Text, nullable=False)
-    vk_id = sq.Column(sq.Integer, nullable=False)
+    vk_id = sq.Column(sq.Text, primary_key=True)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey('user.user_id'))
+    user = relationship(User, backref='user')
 
 class Favourites(Base):
     __tablename__ = 'favourites'
 
-    id = sq.Column(sq.Integer, primary_key=True)
+    fav_id = sq.Column(sq.Integer, primary_key=True)
     first_name = sq.Column(sq.Text, nullable=False)
     last_name = sq.Column(sq.Text, nullable=False)
-    gender = sq.Column(sq.Text, nullable=False)
-    city = sq.Column(sq.Text, nullable=False)
-    his_id = sq.Column(sq.Integer, sq.ForeignKey('history.id'))
-    history = relationship(History, backref='history')
+    vk_link = sq.Column(sq.Text, nullable=False)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey('user.user_id'))
+    user = relationship(User, backref='user')
 
 class Photo(Base):
     __tablename__ = 'photo'
 
-    id = sq.Column(sq.Integer, primary_key=True)
-    link = sq.Column(sq.Text, nullable=False)
-    fav_id = sq.Column(sq.Integer, sq.ForeignKey('favourites.id'))
+    photo_id = sq.Column(sq.Integer, primary_key=True)
+    photo_link = sq.Column(sq.Text, nullable=False)
+    fav_id = sq.Column(sq.Integer, sq.ForeignKey('favourites.fav_id'))
     favourites = relationship(Favourites, backref='favourites')
 
 

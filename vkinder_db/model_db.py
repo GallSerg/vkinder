084139dd7e743d59dc2898427371946fa194242a
e12io898 <e12io898@ya.ru>
2023-07-02 16:13:50 +0300

    02072023

--- vkinder_db/model_db.py
+++ vkinder_db/model_db.py
@@ -6,14 +6,16 @@ Base = declarative_base()
 class History(Base):
     __tablename__ = 'history'
 
-    vk_id = sq.Column(sq.Text, primary_key=True)
-    user_id = sq.Column(sq.Integer, sq.ForeignKey('user.user_id'))
+    id = sq.Column(sq.Integer, primary_key=True)
+    vk_id = sq.Column(sq.Text, nullable=False)
+    user_id = sq.Column(sq.Integer, sq.ForeignKey('user.id'))
 
 class User(Base):
     __tablename__ = 'user'
 
-    user_id = sq.Column(sq.Integer, primary_key=True)
-    firs_name = sq.Column(sq.Text, nullable=False)
+    id = sq.Column(sq.Integer, primary_key=True)
+    user_vk_id = sq.Column(sq.Text, nullable=False)
+    first_name = sq.Column(sq.Text, nullable=False)
     last_name = sq.Column(sq.Text, nullable=False)
     city = sq.Column(sq.Text, nullable=False)
     gender = sq.Column(sq.Text, nullable=False)
@@ -23,19 +25,19 @@ class User(Base):
 class Favourites(Base):
     __tablename__ = 'favourites'
 
-    fav_id = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key=True)
     first_name = sq.Column(sq.Text, nullable=False)
     last_name = sq.Column(sq.Text, nullable=False)
     vk_link = sq.Column(sq.Text, nullable=False)
-    user_id = sq.Column(sq.Integer, sq.ForeignKey('user.user_id'))
+    user_id = sq.Column(sq.Integer, sq.ForeignKey('user.id'))
     favourites_conn = relationship(User, backref='user')
 
 class Photo(Base):
     __tablename__ = 'photo'
 
-    photo_id = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key=True)
     photo_link = sq.Column(sq.Text, nullable=False)
-    fav_id = sq.Column(sq.Integer, sq.ForeignKey('favourites.fav_id'))
+    fav_id = sq.Column(sq.Integer, sq.ForeignKey('favourites.id'))
     photo_conn = relationship(Favourites, backref='favourites')
 
 

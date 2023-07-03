cbf56adbec9be22eb804528db445030f1b1b6bce
Sergei Galchin <warlik.galchin@yandex.ru>
2023-07-03 17:19:51 +0500

    Added get_client and get_realationship methods

--- vk_user.py
+++ vk_user.py
@@ -58,9 +58,34 @@ class VkUser:
             min_v, min_i = self.get_likes(self, three_photos_from_vk_max_likes)
             if len(three_photos_from_vk_max_likes) < 3:
                 three_photos_from_vk_max_likes.append(photo)
-            elif photo['likes']['count'] < min_v:
+            elif photo['likes'] < min_v:
                 three_photos_from_vk_max_likes[min_i] = photo
         return three_photos_from_vk_max_likes
+
+    def get_client(self, user_id):
+        client_url = self.url + 'users.get'
+        client_params = {
+            'user_ids': user_id,
+            'fields': 'sex, city'
+        }
+        response = requests.get(client_url, params={**self.user_params, **client_params})
+        resp = response.json()
+        return resp['response'][0]['first_name'], resp['response'][0]['last_name'], \
+            resp['response'][0]['sex'], resp['response'][0]['city']['id']
+
+    def get_relationship(self, gender=0, city=99999):
+        fam_status = 1
+        client_url = self.url + 'users.search'
+        gender_dict = {1: 2, 2: 1, 0: 0}
+        gender = gender_dict[gender]
+        search_params = {
+            'city': city,
+            'sex': gender,
+            'status': fam_status
+        }
+        response = requests.get(client_url, params={**self.user_params, **search_params})
+        resp = response.json()
+        return resp
 
     def msg_listener(self):
         vk_session = vk_api.VkApi(token=self.group_token)
@@ -73,8 +98,11 @@ class VkUser:
             if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
                 print('id{}: "{}"'.format(event.user_id, event.text), end=' ')
                 attachments = []
+                first_name, last_name, gender, city = self.get_client(event.user_id)
+                print(first_name, last_name, gender, city)
+                print(self.get_relationship(gender, city))
                 user_id = 1
-                self.get_photos(user_id, 'wall')
+                self.get_photos(user_id, 'profile')
                 photos = self.get_photos_params()
                 text = 'Лайков: '
                 if photos:

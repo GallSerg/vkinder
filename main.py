6fc9b3ee8fde9ae16929d0db378abb0e11f427c7
Sergei Galchin <warlik.galchin@yandex.ru>
2023-07-05 10:09:06 +0500

    Changed main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
 import vk_user as vk
 from dotenv import load_dotenv
+from vkinder_db.model_db import create_tables
+from vkinder_db.work_with_db import connect_db, Database
 
 
 if __name__ == '__main__':
     load_dotenv()
+    session, engine = connect_db()
+    create_tables(engine)
+    test = Database()
     group_token = os.environ['vk_group_token']
     user_token = os.environ['vk_user_token']
     vk_client = vk.VkUser(group_token, user_token, '5.131')
